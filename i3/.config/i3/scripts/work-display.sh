f1423552bfc6e35cf3e127563e5ad6cae3bdb94c
ptanguy <philip.tanguy@gmail.com>
2019-05-06 14:55:04 +0200

    fix error new name for display

--- i3/.config/i3/scripts/work-display.sh
+++ i3/.config/i3/scripts/work-display.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1-1 --off --output HDMI-1 --off --output DP-1-3 --off --output DP-1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-2 --off
+xrandr --output eDP1 --off --output DP1-1 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off
